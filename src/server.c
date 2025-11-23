bbff46f2b4f21cbac83e1fe1b4ee58d25787c700
Lliam Symonds <lliamsymonds04@gmail.com>
2025-11-23 14:48:40 +1000

    added server.c

---
+++ src/server.c
@@ -0,0 +1,44 @@
+#include "server.h"
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BACKLOG 5
+
+int create_server_socket(int port) {
+  int sockfd;
+  struct sockaddr_in server_addr;
+  int opt = 1;
+
+  // create socket
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    return -1;
+  }
+
+  // set socket options
+  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+    close(sockfd);
+    return -1;
+  }
+
+  // configure server address struct
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(port);
+
+  // bind socket to the specified port
+  if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    close(sockfd);
+    return -1;
+  }
+
+  // listen for incoming connections
+  if (listen(sockfd, BACKLOG) < 0) {
+    close(sockfd);
+    return -1;
+  }
+
+  return sockfd;
+}
